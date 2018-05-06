2fb8c292330859b8c33f154d138d7c3d58f5433a
TakaakiKihara <Takaaki.Kihara.2018@marshall.usc.edu config --global user.name TakaakiKihara>
2018-05-06 02:17:29 -0700

    Cuz the original data was too big to run the code, limit the data based on trade parnerships.
    So far, created shiny for TPP and NAFTA.

--- Agriculture.R
+++ Agriculture.R
@@ -1,21 +1,26 @@
 ### Overlook of Agricultural Production
+library(data.table)
 library(shiny)
 library(ggplot2)
 library(dplyr)
 library(shiny)
 library(tidyr)
 library(stringr)
+library(mapproj)
 
-production = read.csv("Production_Crops_E_All_Data.csv") 
+
+production = fread("Production_Crops_E_All_Data.csv") 
 
 production = production %>%
   select(-ends_with("F")) %>%
   gather(Y1961:Y2016, key = "Year", value = "production") 
 
 production$Year = str_sub(production$Year,2)
+production$Year = as.numeric(production$Year)
+production$Area = as.character(production$Area)
 
 production_TPP = production %>%
-  filter(Area == c("United States of America",
+  filter(Area %in% c("United States of America",
                    "Japan",
                    "Canada",
                    "Mexico",
@@ -26,33 +31,72 @@ production_TPP = production %>%
                    "Brunei Darussalam",
                    "Singapore",
                    "Malaysia",
-                   "Vietnam",
-                   "Thailand"))
+                   "Viet Nam",
+                   "Thailand") &
+           Item %in% c("Rice, paddy",
+                       "Cereals (Rice Milled Eqv)",
+                       "Wheat",
+                       "Barley",
+                       "Maize",
+                       "Soybeans",
+                       "Sugar beet",
+                       "Sugar cane",
+                       "Oilcrops, Oil Equivalent") &
+           Element == "Production" &
+           Year %in% c(1961, seq(1960,2010,10),2016)) 
 
 production_NAFTA = production %>%
-  filter(Area == c("United States of America",
-                  "Canada",
-                  "Mexico"))
-
-production_TPP$Area = as.character(production_TPP$Area)
+  filter(Area %in% c("United States of America",
+                     "Canada",
+                     "Mexico"))
 
 world_map = map_data("world")
-world_map$region = as.factor(world_map$region)
+world_map$region[world_map$region == "Brunei"] = "Brunei Darussalam"
+world_map$region[world_map$region == "USA"] = "United States of America"
+world_map$region[world_map$region == "Vietnam"] = "Viet Nam"
+
+production_TPP_map = right_join(world_map, production_TPP,
+                               by = c("region" = "Area"))
 
 
-production_NAFTA_map = right_join(world_map, production_NAFTA,
-                           by = c("region" = "Area"))
+production_NAFTA_map = full_join(world_map, production_NAFTA,
+                                 by = c("region" = "Area"))
+
+
+
+production_TPP_map %>%
+  filter(Item == "Wheat" & Year == 2010) %>%
+  ggplot(aes(x = long,
+           y= lat,
+           group = group,
+           fill = production)) +
+  geom_polygon() +
+  scale_fill_gradient(low = "white",
+                      high = "red") 
+
 
 ui <- fluidPage(
-  titlePanel(title = "World Agricltural Production",
-             windowTitle = "Agri Pro"),
+  titlePanel(title = "TPP countires Agricltural Production",
+             windowTitle = "TPP"),
   sidebarLayout(
     sidebarPanel(
       helpText("Create agricultural production map"),
-      selectInput(inputId = "selected_var",
-                  label = "Chooose a variable to display",
-                  choices = 
-                  selected = "Percent Latino")
+      selectInput(inputId = "Item",
+                  label = "Chooose an Item to display",
+                  choices = c("Rice, paddy",
+                              "Cereals (Rice Milled Eqv)",
+                              "Wheat",
+                              "Barley",
+                              "Maize",
+                              "Soybeans",
+                              "Sugar beet",
+                              "Sugar cane",
+                              "Oilcrops, Oil Equivalent"),
+                  selected = ""),
+      selectInput(inputId = "Year",
+                  label = "Chooose a Year to display",
+                  choices = c(1961, seq(1960,2010,10),2016),
+                  selected = "")
     ),
     mainPanel(
       textOutput(outputId = "map_title"),
@@ -64,7 +108,64 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  
+  output$map = renderPlot({
+    production_TPP_map = ({
+      production = fread("Production_Crops_E_All_Data.csv") 
+      
+      production = production %>%
+        select(-ends_with("F")) %>%
+        gather(Y1961:Y2016, key = "Year", value = "production") 
+      
+      production$Year = str_sub(production$Year,2)
+      production$Year = as.numeric(production$Year)
+      production$Area = as.character(production$Area)
+      
+      production_TPP = production %>%
+        filter(Area %in% c("United States of America",
+                           "Japan",
+                           "Canada",
+                           "Mexico",
+                           "Peru",
+                           "Chile",
+                           "New Zealand",
+                           "Australia",
+                           "Brunei Darussalam",
+                           "Singapore",
+                           "Malaysia",
+                           "Viet Nam",
+                           "Thailand") &
+                 Item %in% c("Rice, paddy",
+                             "Cereals (Rice Milled Eqv)",
+                             "Wheat",
+                             "Barley",
+                             "Maize",
+                             "Soybeans",
+                             "Sugar beet",
+                             "Sugar cane",
+                             "Oilcrops, Oil Equivalent") &
+                 Element == "Production" &
+                 Year %in% c(1961, seq(1960,2010,10),2016)) 
+      
+      world_map = map_data("world")
+      world_map$region[world_map$region == "Brunei"] = "Brunei Darussalam"
+      world_map$region[world_map$region == "USA"] = "United States of America"
+      world_map$region[world_map$region == "Vietnam"] = "Viet Nam"
+      
+      right_join(world_map, production_TPP,
+                 by = c("region" = "Area"))
+
+    })
+    
+    production_TPP_map %>%
+      filter(Item == input$Item & Year == input$Year) %>%
+    ggplot(aes(x = long,
+               y= lat,
+               group = group,
+               fill = production)) +
+      geom_polygon() +
+      scale_fill_gradient(low = "white",
+                          high = "red")
+    })
 }
 
 shinyApp(ui, server)
